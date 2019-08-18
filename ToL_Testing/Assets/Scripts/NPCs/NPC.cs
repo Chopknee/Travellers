153c31331654b16cacc706196350fac65f5b172b
Drennen Dooms <ddooms@iu.edu>
2019-08-18 18:03:20 -0400

    NPC groundwork

---
+++ ToL_Testing/Assets/Scripts/NPCs/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public NPCAttributes attributes;
+    public float health;
+
+    private void Awake()
+    {
+        attributes = new NPCAttributes(100, 10, 1, NPCAttributes.Difficulty.Easy, NPCAttributes.DamageType.Melee, true);
+    }
+}
