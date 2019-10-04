b6a256df8ca3bac2a3ac6f9ed1f8f4dd8f061136
Chopknee <Chopknee@gmail.com>
2019-10-04 19:08:38 -0400

    Syncing work.

--- Prototypes/WorldGen/Assets/Scripts/Player/Combat/AttackController/CombatController.cs
+++ Prototypes/WorldGen/Assets/Scripts/Player/Combat/AttackController/CombatController.cs
@@ -2,81 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Photon.Pun;
 
-public class CombatController : MonoBehaviour, IPunObservable
-{
+public class CombatController : MonoBehaviour {
     Animator anim;
-    public TextMeshProUGUI t;
 
     public GameObject currentWeapon;
-    Collider hitbox;
+    AttackStyle weaponScript;
 
-    float lastHit;
 
-    PhotonView view;
-    bool attacking = false;
-    public bool canAttack = false;
-
-    /*
-     * make movement and attack the same button, and only attack if clicking on an enemy.
-     * 
-     */
 
     private void Start() {
-        anim = GetComponent<Animator>();
-        hitbox = currentWeapon.transform.Find("Hitbox").GetComponent<Collider>();
-        view = GetComponent<PhotonView>();
-        if (!view.IsMine) {
-            canAttack = true;
+        if (currentWeapon.GetComponent<AttackStyle>() == null) {
+            Debug.Log("Cannot use current weapon gameobject. It does not have an attack style or derivative script.");
+            return;
         }
+        SetWeapon(currentWeapon.GetComponent<AttackStyle>());
     }
 
-    // Update is called once per frame
     void Update() {
-        if (view.IsMine && canAttack) {
-            if (Input.GetButtonDown("Attack") && Time.time - lastHit > hitbox.transform.parent.GetComponent<MeleeWeapon>().attackRate) {
-                if (t != null) {
-                    t.text = "Attacking";
+        if (currentWeapon != null) {
+            if (Input.GetButtonDown("Attack")) {
+                if (!weaponScript.IsAttacking) {
+                    weaponScript.DoAttack();
+                    //Debug.Log("Attacking!!");
+                } else {
+                    //Debug.Log("Can't attack, already attacking.");
                 }
-                anim.SetTrigger("Attack");
-                attacking = true;
-            }
-        }
-
-        if (NetInstanceManager.CurrentManager.isInstanceMaster) {
-            if (attacking) {
-                Invoke("BeginCollision", 0.7f);
             }
         }
     }
 
-    //This should only be run on the master of the instance
-    void BeginCollision() {
-        if (hitbox != null) {
-            hitbox.enabled = true;
-        }
-        Invoke("EndCollision", .5f);
-    }
-    
-    void EndCollision() {
-        if (t != null) {
-            t.text = "";
-        }
-        if (hitbox != null)
-            hitbox.enabled = false;
-
-        attacking = false;
-    }
-
-    public void OnPhotonSerializeView ( PhotonStream stream, PhotonMessageInfo info ) {
-        if (view != null) {
-            if (view.IsMine) {
-                //I'm sending the messages now bitch!
-                stream.SendNext(attacking);
-            } else if (!view.IsMine && !attacking) {
-                attacking = (bool) stream.ReceiveNext();
-            }
-        }
+    public void SetWeapon(AttackStyle weapon) {
+        currentWeapon = weapon.gameObject;
+        weaponScript = weapon;
+        weaponScript.wielder = gameObject;
     }
 }
